f9887235ae650cfac07c11a99b37ee4185a7eeb0
dennis <den.deufemia@gmail.com>
2026-06-11 18:12:44 +0200

    sistemato bug hamburgermenu e migliorato l'interfaccia

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let hamburgerButton = document.getElementById("hamburger");
 let mainNav = document.getElementById('main-nav');
 let styleRules = document.styleSheets[0].cssRules;
 let navigationElement = document.querySelector('.navigation');
+let isDesktop = window.matchMedia("(min-width: 768px)");
+let isdark=0;
 
 hamburgerButton.addEventListener('click', function() {
   hamburgerButton.classList.toggle('closed');
@@ -15,7 +17,9 @@ hamburgerButton.addEventListener('click', function() {
 })
 
 mainNav.addEventListener('click', () => {
+  if (!isDesktop.matches) {
   hamburgerButton.classList.add('closed');
   navigationElement.style.opacity = '0';
     navigationElement.style.transform = 'scale(0)';
+}
 })
